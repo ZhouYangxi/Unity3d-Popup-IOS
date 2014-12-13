8b0b7820e041c5a5d32e5719660f4a9b5d752ac6
ZhouYangxi <zhou.yx@outlook.com>
2014-12-14 00:35:36 +0800

    添加功能
    调用AlertView

--- Popup/Popup.h
+++ Popup/Popup.h
@@ -7,11 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface Popup : NSObject
 
 +(Popup *)Instance;
+-(void) dealloc;
 
 extern void UnitySendMessage(const char* _objName, const char* _callBackName, const char* _paramsInString);
+
+
+
+
+void ShowAlertView(const char* _title, const char * _message, const char * _confirmText);
+-(void) ShowAlertView : (NSString *) _title Message: (NSString *) _message ConfirmText : (NSString *) _confirmText;
+
+void ShowAlertViewWithTwoButtons(const char* _title, const char * _message, const char * _cancelText, const char * _okText);
+-(void) ShowAlertView:(NSString *)_title Message:(NSString *)_message CancelText:(NSString *)_cancelText OkText:(NSString *)_okText;
+
 
 @end
--- Popup/Popup.m
+++ Popup/Popup.m
@@ -25,4 +25,45 @@
     return instance;
 }
 
+-(void) dealloc
+{
+    [super dealloc];
+}
+
+void ShowAlertView(const char* _title, const char * _message, const char * _confirmText)
+{
+    NSString * str1 = [NSString stringWithUTF8String:_title];
+    NSString * str2 = [NSString stringWithUTF8String:_message];
+    NSString * str3 = [NSString stringWithUTF8String:_confirmText];
+    
+    [[Popup Instance] ShowAlertView:str1 Message:str2 ConfirmText:str3];
+}
+
+void ShowAlertViewWithTwoButtons(const char* _title, const char * _message, const char * _cancelText, const char * _okText)
+{
+    NSString * str1 = [NSString stringWithUTF8String:_title];
+    NSString * str2 = [NSString stringWithUTF8String:_message];
+    NSString * str3 = [NSString stringWithUTF8String:_cancelText];
+    NSString * str4 = [NSString stringWithUTF8String:_okText];
+
+    [[Popup Instance]ShowAlertView:str1 Message:str2 CancelText:str3 OkText:str4];
+}
+
+-(void) ShowAlertView:(NSString *)_title Message:(NSString *)_message ConfirmText:(NSString *)_confirmText
+{
+    UIAlertView * av = [[UIAlertView alloc] initWithTitle:_title message:_message delegate:self cancelButtonTitle:_confirmText otherButtonTitles: nil];
+    
+    [av show];
+    
+    [av release];
+}
+
+-(void)ShowAlertView:(NSString *)_title Message:(NSString *)_message CancelText:(NSString *)_cancelText OkText:(NSString *)_okText
+{
+    UIAlertView * av = [[UIAlertView alloc] initWithTitle:_title message:_message delegate:self cancelButtonTitle:_cancelText otherButtonTitles: _okText,nil];
+    
+    [av show];
+    
+    [av release];
+}
 @end
